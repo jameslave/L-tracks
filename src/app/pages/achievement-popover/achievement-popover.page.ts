841570a1820dc729269372a756e74f531f9afe86
James Lave <jlave.dev@gmail.com>
2019-04-11 15:49:07 -0500

    Replace confetti script with confetti-js package

--- src/app/pages/achievement-popover/achievement-popover.page.ts
+++ src/app/pages/achievement-popover/achievement-popover.page.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AchievementsService } from 'src/app/services/achievements.service';
 import Achievement from 'src/app/models/Achievement';
 import { NavController } from '@ionic/angular';
-import renderConfetti from '../../utils/render-confetti';
+import 'confetti-js';
 
 @Component({
   selector: 'app-achievement-popover',
@@ -23,7 +23,27 @@ export class AchievementPopoverPage implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    renderConfetti();
+    const canvas: HTMLCanvasElement = document.getElementById('confetti-holder') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    const confettiConfig = {
+      max: 30,
+      clock: 20,
+      rotate: true,
+      props: ['circle', 'square', 'triangle'],
+      animate: true,
+    };
+    let confetti = new (window as any).ConfettiGenerator(confettiConfig);
+    confetti.render();
+
+    window.addEventListener('resize', (e) => {
+      confetti.clear();
+      confetti = new (window as any).ConfettiGenerator(confettiConfig);
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      canvas.style.width = `${window.innerWidth}px`;
+      canvas.style.height = `${window.innerHeight}px`;
+      confetti.render();
+    });
   }
 
   get achievementName() {
